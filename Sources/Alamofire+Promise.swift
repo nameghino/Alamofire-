09e048c0c2e3346592d9939a911c42dc0323b401
Max Howell <mxcl@me.com>
2017-01-31 13:47:18 -0500

    Update to PMK5; Refine APIs

--- Sources/Alamofire+Promise.swift
+++ Sources/Alamofire+Promise.swift
@@ -1,8 +1,7 @@
-@_exported import Alamofire
-import Foundation
-#if !COCOAPODS
 import PromiseKit
-#endif
+import Alamofire
+
+public enum PMKAFResponseOptions { case raw }
 
 /**
  To import the `Alamofire` category:
@@ -13,155 +12,159 @@ import PromiseKit
  And then in your sources:
 
      import PromiseKit
+
  */
 extension Alamofire.DataRequest {
-    /// Adds a handler to be called once the request has finished.
-    public func response() -> Promise<(URLRequest, HTTPURLResponse, Data)> {
-        return Promise { fulfill, reject in
+
+    public func response(_: PMKNamespacer) -> Promise<Void> {
+        return Promise { seal in
             response(queue: nil) { rsp in
                 if let error = rsp.error {
-                    reject(error)
-                } else if let a = rsp.request, let b = rsp.response, let c = rsp.data {
-                    fulfill(a, b, c)
+                    seal.reject(error)
                 } else {
-                    reject(PMKError.invalidCallingConvention)
+                    seal.fulfill()
                 }
             }
         }
     }
 
-    /// Adds a handler to be called once the request has finished.
+    /**
+          firstly {
+              Alamofire.request(url).responseData(with: .raw)
+          }.then { data, rsp in
+              print(rsp.request)
+          }
+     */
+    public func responseData(with: PMKAFResponseOptions) -> Promise<(Data, Alamofire.DataResponse<Data>)> {
+        return Promise { seal in
+            responseData(queue: nil) { rsp in
+                if let error = rsp.error {
+                    seal.reject(error)
+                } else if let data = rsp.data {
+                    seal.fulfill(data, rsp)
+                } else {
+                    seal.reject(PMKError.invalidCallingConvention)
+                }
+            }
+        }
+    }
+
+    /**
+          firstly {
+              Alamofire.request(url).responseData()
+          }.then { data in
+              //…
+          }
+     */
     public func responseData() -> Promise<Data> {
-        return Promise { fulfill, reject in
-            responseData(queue: nil) { response in
+        return responseData(with: .raw).map(on: nil){ $0.0 }
+    }
+
+    /**
+          firstly {
+              Alamofire.request(url).responseString(with: .raw)
+          }.then { string, rsp in
+              print(rsp.request)
+          }
+     */
+    public func responseString(with: PMKAFResponseOptions, encoding: String.Encoding? = nil) -> Promise<(String, Alamofire.DataResponse<String>)> {
+        return Promise { pipe in
+            responseString(queue: nil, encoding: encoding) { response in
                 switch response.result {
                 case .success(let value):
-                    fulfill(value)
+                    pipe.fulfill(value, response)
                 case .failure(let error):
-                    reject(error)
+                    pipe.reject(error)
                 }
             }
         }
     }
 
-    /// Adds a handler to be called once the request has finished.
-    public func responseString() -> Promise<String> {
-        return Promise { fulfill, reject in
-            responseString(queue: nil) { response in
+    /**
+          firstly {
+              Alamofire.request(url).responseString()
+          }.then { string in
+              //…
+          }
+     */
+    public func responseString(encoding: String.Encoding? = nil) -> Promise<String> {
+        return responseString(with: .raw, encoding: encoding).map(on: nil){ $0.0 }
+    }
+
+    /**
+          firstly {
+              Alamofire.request(url).responseJSON(with: .raw)
+          }.then { json, rsp in
+              print(rsp.request)
+          }
+     */
+    public func responseJSON(with: PMKAFResponseOptions, options: JSONSerialization.ReadingOptions = .allowFragments) -> Promise<(Any, Alamofire.DataResponse<Any>)> {
+        return Promise { seal in
+            responseJSON(queue: nil, options: options, completionHandler: { response in
                 switch response.result {
                 case .success(let value):
-                    fulfill(value)
+                    seal.fulfill(value, response)
                 case .failure(let error):
-                    reject(error)
+                    seal.reject(error)
                 }
-            }
+            })
         }
     }
 
-    /// Adds a handler to be called once the request has finished.
+    /**
+          firstly {
+              Alamofire.request(url).responseJSON().flatMap{ $0 as? [String: Any] }
+          }.then { json in
+              // NOTE! The recommended use of `Promise.flatMap(_:)`
+          }
+     */
     public func responseJSON(options: JSONSerialization.ReadingOptions = .allowFragments) -> Promise<Any> {
-        return Promise { fulfill, reject in
-            responseJSON(queue: nil, options: options, completionHandler: { response in
-                switch response.result {
-                case .success(let value):
-                    fulfill(value)
-                case .failure(let error):
-                    reject(error)
-                }
-            })
-        }
-    }
-
-    /// Adds a handler to be called once the request has finished. Provides access to the detailed response object.
-    ///    request.responseJSON(with: .response).then { json, response in }
-    public func responseJSON(with: PMKAlamofireOptions, options: JSONSerialization.ReadingOptions = .allowFragments) -> Promise<(Any, PMKDataResponse)> {
-        return Promise { fulfill, reject in
-            responseJSON(queue: nil, options: options, completionHandler: { response in
-                switch response.result {
-                case .success(let value):
-                    fulfill(value, PMKDataResponse(response))
-                case .failure(let error):
-                    reject(error)
-                }
-            })
-        }
-    }
-
-
-    /// Adds a handler to be called once the request has finished and the resulting JSON is rooted at a dictionary.
-    public func responseJsonDictionary(options: JSONSerialization.ReadingOptions = .allowFragments) -> Promise<[String: Any]> {
-        return Promise { fulfill, reject in
-            responseJSON(queue: nil, options: options, completionHandler: { response in
-                switch response.result {
-                case .success(let value):
-                    if let value = value as? [String: Any] {
-                        fulfill(value)
-                    } else {
-                        reject(JSONError.unexpectedRootNode(value))
-                    }
-                case .failure(let error):
-                    reject(error)
-                }
-            })
-        }
-
-    }
-
-    /// Adds a handler to be called once the request has finished.
-    public func responsePropertyList(options: PropertyListSerialization.ReadOptions = PropertyListSerialization.ReadOptions()) -> Promise<Any> {
-        return Promise { fulfill, reject in
-            responsePropertyList(queue: nil, options: options) { response in
-                switch response.result {
-                case .success(let value):
-                    fulfill(value)
-                case .failure(let error):
-                    reject(error)
-                }
-            }
-        }
+        return responseJSON(with: .raw).map(on: nil){ $0.0 }
     }
 }
 
 extension Alamofire.DownloadRequest {
-    /// Adds a handler to be called once the request has finished.
-    public func responseData() -> Promise<DownloadResponse<Data>> {
-        return Promise { fulfill, reject in
+    /**
+          firstly {
+              Alamofire.download(url).responseData(with: .raw)
+          }.then { data, rsp in
+              print(rsp.request)
+          }
+     */
+    public func responseData(with: PMKAFResponseOptions) -> Promise<(Data, Alamofire.DownloadResponse<Data>)> {
+        return Promise { seal in
             responseData(queue: nil) { response in
                 switch response.result {
-                case .success:
-                    fulfill(response)
+                case .success(let value):
+                    seal.fulfill(value, response)
                 case .failure(let error):
-                    reject(error)
+                    seal.reject(error)
                 }
             }
         }
     }
-}
 
-
-
-public enum PMKAlamofireOptions {
-    case response
-}
-
-
-public struct PMKDataResponse {
-    fileprivate init(_ rawrsp: Alamofire.DataResponse<Any>) {
-        request = rawrsp.request
-        response = rawrsp.response
-        data = rawrsp.data
-        timeline = rawrsp.timeline
+    /**
+          firstly {
+              Alamofire.download(url).responseData()
+          }.then { data in
+              //…
+          }
+     */
+    public func responseData() -> Promise<Data> {
+        return responseData(with: .raw).map(on: nil){ $0.0 }
     }
-
-    /// The URL request sent to the server.
-    public let request: URLRequest?
-
-    /// The server's response to the URL request.
-    public let response: HTTPURLResponse?
-
-    /// The data returned by the server.
-    public let data: Data?
-
-    /// The timeline of the complete lifecycle of the request.
-    public let timeline: Timeline
+    
+    /**
+     Feeds you your destinationUrl or, if unset, the temporaryURL.
+     This variant is more efficient since the `Data` of the download is not allocated into memory.
+    */
+    public func response(_: PMKNamespacer) -> Promise<URL> {
+        return Promise { seal in
+            response(queue: nil) { rsp in
+                let url = rsp.destinationURL ?? rsp.temporaryURL
+                seal.resolve(value: url, error: rsp.error)
+            }
+        }
+    }
 }
--- Tests/TestAlamofire.swift
+++ Tests/TestAlamofire.swift
@@ -1,20 +1,25 @@
 import PMKAlamofire
 import OHHTTPStubs
 import PromiseKit
+import Alamofire
 import XCTest
 
 class AlamofireTests: XCTestCase {
     func test() {
-        let json: NSDictionary = ["key1": "value1", "key2": ["value2A", "value2B"]]
+        let inputJson: [String: Any] = ["key1": "value1", "key2": ["value2A", "value2B"]]
 
         OHHTTPStubs.stubRequests(passingTest: { $0.url!.host == "example.com" }) { _ in
-            return OHHTTPStubsResponse(jsonObject: json, statusCode: 200, headers: nil)
+            return OHHTTPStubsResponse(jsonObject: inputJson, statusCode: 200, headers: nil)
         }
 
         let ex = expectation(description: "")
 
-        let rq = Alamofire.request("http://example.com", method: .get).responseJSON().then { rsp -> Void in
-            XCTAssertEqual(json, rsp as? NSDictionary)
+        firstly {
+            Alamofire.request("http://example.com").responseJSON()
+        }.flatMap {
+            $0 as? [String: Any]
+        }.done { outputJson in
+            XCTAssertEqual(inputJson as NSDictionary, outputJson as NSDictionary)
             ex.fulfill()
         }
         waitForExpectations(timeout: 1)
